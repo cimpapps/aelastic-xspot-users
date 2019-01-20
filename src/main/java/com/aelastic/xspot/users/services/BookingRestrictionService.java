e901df5b9e0ee594cb9f98a25c0b7bff98da44e8
cimpapps <catalincimpoeru24@gmail.com>
2019-01-20 23:46:00 +0200

    sketch bookingRestrictionService

---
+++ src/main/java/com/aelastic/xspot/users/services/BookingRestrictionService.java
@@ -0,0 +1,11 @@
+package com.aelastic.xspot.users.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+//kafka listener...listen to did_not_showed_up
+public class BookingRestrictionService {
+    //TODO implement //listen to booking service...when a booking is reserved with did_not_showed_up
+
+
+}
